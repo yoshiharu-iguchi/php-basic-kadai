d06ecd48e895754a930d8ace7c28112fa9488090
yoshiharu-iguchi <67188365+yoshiharu-iguchi@users.noreply.github.com>
2025-03-05 09:55:00 +0900

    kadai_007作成

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="ja">
+  <head>
+    <title>PHP基礎編</title>
+  </head>
+  <body>
+    <?php
+    $Array = ['name' => 'onion','price' => 200,'weight' => 160];
+
+    print_r($Array);
+    ?>
+  </body>
+</html>
